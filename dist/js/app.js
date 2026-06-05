e75d6dd9bcc153d8079bf779ca14a1a6e84028bb
Robert Recopelacion <rrecopelacion@revolutionparts.com>
2026-06-05 14:50:16 +0800

    Updates

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,4 +10,45 @@ $(document).ready(function() {
         $('.ptz_HeaderNavigationWrap').removeClass('active');
         $('body').removeClass('openMenu');
     })
+    $('.ptz_HeaderCloseMenu').on('click', function() {
+        $(this).removeClass('active');
+        $('.ptz_HeaderNavigationWrap').removeClass('active');
+        $('body').removeClass('openMenu');
+    })
+    // sticky on scroll
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 350){  
+            $('header.ptz_Header').addClass("sticky");
+            $('body').addClass('sticky')
+        }
+        else {
+            $('header.ptz_Header').removeClass("sticky");
+            $('body').removeClass('sticky')
+        }
+    });
+    //brand logo carousel
+    $("#ptz_BrandlogoList").owlCarousel({
+        loop: true,
+        margin: 50,
+        nav: false,
+        dots: false,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 4,
+                margin: 20,
+            },
+            768: {
+                items: 5
+            },
+            991: {
+                items: 6
+            }
+        }
+    });
 })
